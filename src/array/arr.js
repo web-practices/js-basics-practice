693fb70306ccb52464d8f45827ae2a29a45852fb
Eason Li <lijinghua@odc.cmbchina.cn>
2020-02-28 18:37:55 +0800

    Add Array practice

---
+++ src/array/arr.js
@@ -0,0 +1,72 @@
+function joinArrays(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+function checkAdult(arr, age) {
+  return arr.every((age) => age >= 18);
+}
+
+function findAdult(arr, age) {
+  return arr.find((age) => age >= 18);
+}
+
+function convertArrToStr(arr, str) {
+  return arr.join(str);
+}
+
+function removeLastEle(arr) {
+  arr.pop();
+  return arr;
+}
+
+function addNewItem(arr, item) {
+  arr.push(item);
+  return arr;
+}
+
+function removeFirstItem(arr) {
+  arr.shift();
+  return arr;
+}
+
+function addNewItemToBeginArr(arr, item) {
+  arr.unshift(item);
+  return arr;
+}
+
+function reverseOrder(arr) {
+  return arr.reverse();
+}
+
+function selectElements(arr, start, end) {
+  return arr.slice(start, end);
+}
+
+function addItemsToArray(arr, index, howmany, item) {
+  arr.splice(index, howmany, item);
+  return arr;
+}
+
+function sortASC(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+function sortDESC(arr) {
+  return arr.sort((a, b) => b - a);
+}
+
+module.exports = {
+  joinArrays,
+  checkAdult,
+  findAdult,
+  convertArrToStr,
+  removeLastEle,
+  addNewItem,
+  removeFirstItem,
+  addNewItemToBeginArr,
+  reverseOrder,
+  selectElements,
+  addItemsToArray,
+  sortASC,
+  sortDESC
+};
