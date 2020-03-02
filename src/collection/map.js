d0a05421fd7880c5a549f9890b37f045871880ea
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-02 18:36:01 +0800

    Add test for array map

---
+++ src/collection/map.js
@@ -0,0 +1,23 @@
+function doubleItem(collection) {
+  // Need to be implemented
+}
+
+function doubleEvenItem(collection) {
+  // Need to be implemented
+}
+
+function covertToCharArray(collection) {
+  // Need to be implemented
+}
+
+function getOneClassScoreByASC(collection) {
+  // Need to be implemented
+}
+
+export {
+  doubleItem,
+  doubleEvenItem,
+  covertToCharArray,
+  getOneClassScoreByASC,
+  countItems
+};
