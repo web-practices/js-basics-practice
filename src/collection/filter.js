6d7042cec7b96327edcc97d50107b82ab3504a32
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-02 17:15:40 +0800

    Implement filter test

--- src/collection/filter.js
+++ src/collection/filter.js
@@ -1,17 +1,20 @@
 function getAllEvens(collection) {
-  // Need to be implemented
+  return collection.filter((item) => item % 2 == 0);
 }
 
 function getAllIncrementEvens(start, end) {
-  // Need to be implemented
+  const origin = Array.from(new Array(end + 1).keys()).slice(start);
+  return getAllEvens(origin);
 }
 
 function getIntersectionOfcollections(collection1, collection2) {
-  // Need to be implemented
+  return collection1.filter((item) => collection2.includes(item));
 }
 
 function getUnionOfcollections(collection1, collection2) {
-  // Need to be implemented
+  return collection1.concat(
+    collection2.filter((item) => !collection1.includes(item))
+  );
 }
 
 export {
